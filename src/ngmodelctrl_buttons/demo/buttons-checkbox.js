32da6b9c75161d468ecce39b8a34ee164770822c
Igor Minar <igor@angularjs.org>
2013-11-10 12:33:29 +0100

    button checkbox: crazy rewrite.. how about doing it this way

--- src/ngmodelctrl_buttons/demo/buttons-checkbox.js
+++ src/ngmodelctrl_buttons/demo/buttons-checkbox.js
@@ -1,32 +1,20 @@
 angular.module('bs.buttons-checkbox', [])
 
+//shouldn't this be called button-toggle instead?
 .directive('bsBtnCheckbox', function () {
 
   return {
-    require: 'ngModel',
+    scope: {
+      active: '=ngModel'
+    },
+    template: '<button ng-class="{active: active}" ng-click="active = !active" x="{{active}}" ng-transclude>"></button>',
+    replace: true,
+    transclude: true,
     link: function (scope, element, attrs, ngModelCtrl) {
-
-      function getTrueValue() {
-        var trueValue = scope.$eval(attrs.bsBtnCheckboxTrue);
-        return angular.isDefined(trueValue) ? trueValue : true;
-      }
-
-      function getFalseValue() {
-        var falseValue = scope.$eval(attrs.bsBtnCheckboxFalse);
-        return angular.isDefined(falseValue) ? falseValue : false;
-      }
-
-      //model -> UI
-      ngModelCtrl.$render = function () {
-        element.toggleClass('active', angular.equals(ngModelCtrl.$modelValue, getTrueValue()));
-      };
-
-      //ui->model
       element.on('click', function () {
-        scope.$apply(function () {
-          ngModelCtrl.$setViewValue(element.hasClass('active') ? getFalseValue() : getTrueValue());
-          ngModelCtrl.$render();
-        });
+//        scope.$apply(function () {
+//          //scope.active = !scope.active;
+//        });
       });
     }
   };
